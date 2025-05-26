2489b76a1c6588e5b466fac4d9aeb773d80ca985
GeorgeProgramer6142 <karpovgeorgij4@gmail.com>
2025-05-26 16:36:43 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
 
 API_TOKEN = '7212471955:AAGDrPy0bTq_4nEPCeDLaNf6IqFW66gsveM'
 ADMIN_ID = 2018774297
-DB_FILE = 'TelegramBots/onthebackdesk/users_db.json'
-COMPLAINTS_FILE = 'TelegramBots/onthebackdesk/complaints.json'
+DB_FILE = 'users_db.json'
+COMPLAINTS_FILE = 'complaints.json'
