6922ca4a947fcf2b6333005217078da288ccb816
GeorgeProgramer6142 <karpovgeorgij4@gmail.com>
2025-05-26 16:35:47 +0300

    Add files via upload

---
+++ config.py
@@ -0,0 +1,5 @@
+
+API_TOKEN = '7212471955:AAGDrPy0bTq_4nEPCeDLaNf6IqFW66gsveM'
+ADMIN_ID = 2018774297
+DB_FILE = 'TelegramBots/onthebackdesk/users_db.json'
+COMPLAINTS_FILE = 'TelegramBots/onthebackdesk/complaints.json'
